0acf81112e809a943adeffdfc7ff6ba7d878428e
Tetrachlorosilane <146700263+Tetrachlorosilane@users.noreply.github.com>
2026-09-15 01:13:33 +0800

    客户端版本号 1.1.0 -> 1.2.0（本次随对局回放一起发布）

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char* argv[])
 
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName(QStringLiteral("mahjong-client"));
-    QCoreApplication::setApplicationVersion(QStringLiteral("1.1.0"));
+    QCoreApplication::setApplicationVersion(QStringLiteral("1.2.0"));
 
     const QStringList args = QCoreApplication::arguments();
 
